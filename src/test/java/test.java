0f47e86078e40eda5b6bc1e4f76234e96c3c4971
iBodik <seveliukbohdan@gmail.com>
2024-02-14 20:01:12 -0500

    dvnjjdvb

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,5 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("I am batch18 student");
+        System.out.println("hsvdjhbs");
+        System.out.println("kjdjsdkjn");
     }
 }
